707f008e815834e3b76ed68d9c23ec2538d67ec7
Connor Brennan <connorjb@udel.edu>
2026-03-08 20:45:06 -0400

    Finished CycleHoliday component

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,34 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🐇");
+    function holidayByAlphabet(): void {
+        setHoliday(
+            holiday === "🎁" ? "🐇"
+            : holiday === "🐇" ? "🎃"
+            : holiday === "🎃" ? "🎆"
+            : holiday === "🎆" ? "🦃"
+            : "🎁",
+        );
+    }
+    function holidayByYear(): void {
+        setHoliday(
+            holiday === "🐇" ? "🎆"
+            : holiday === "🎆" ? "🎃"
+            : holiday === "🎃" ? "🦃"
+            : holiday === "🦃" ? "🎁"
+            : "🐇",
+        );
+    }
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>{" "}
+            <span>
+                <Button onClick={holidayByAlphabet}>Advance By Alphabet</Button>
+            </span>
+            <span>
+                <Button onClick={holidayByYear}>Advance By Year</Button>
+            </span>
+        </div>
+    );
 }
