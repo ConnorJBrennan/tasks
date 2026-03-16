11750335057aaaf4389a2539986f104f4570c619
Connor Brennan <connorjb@udel.edu>
2026-03-15 21:36:43 -0400

    CheckAnswer and GiveAttempts completed

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
-
+import { Button, Form } from "react-bootstrap";
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [reqestedAttempts, setRequested] = useState<string>("");
+    const reqAttempts = parseInt(reqestedAttempts) || 0;
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>Attempts left: {attempts}</div>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts - 1);
+                }}
+                disabled={attempts === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts + reqAttempts);
+                }}
+            >
+                gain
+            </Button>
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Label>Attempts Requested:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={reqestedAttempts}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setRequested(event.target.value);
+                    }}
+                />
+            </Form.Group>
         </div>
     );
 }
